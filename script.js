1f6176b4fba7b79c938ffcc583774d5684fd17de
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-24 15:59:28 +0100

    end practice

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log('JS OK');
+
+//Photo by < a href = "https://unsplash.com/@borisworkshop?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" > Boris  Smokrovic</a > on < a href = "https://unsplash.com/s/photos/birds?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" > Unsplash</a >
+
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data. : cornice_con_disegno:
+
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            text: 'Hello World using Vue JS!',
+            image: 'boris-smokrovic-HWwF4OnXAdM-unsplash.jpg'
+        },
+        methods: {
+            
+        }
+    }
+);
